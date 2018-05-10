5246ac1a7ac40be3783c90a081ee6c5c33f46d03
AMIN HEYDARI <aheydari@10-148-141-212.dhcp-m.deakin.edu.au>
2018-05-10 12:11:40 +1000

    Initial Commit

---
+++ toolkit_assignment_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toolkit_assignment_2
+//
+//  Created by AMIN HEYDARI on 10/05/2018.
+//  Copyright © 2018 AMIN HEYDARI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
